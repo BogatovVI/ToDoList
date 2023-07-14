12bdc07e696262bf19af4f336d1c53b7bc1bb9f4
BogatovVI <vladosdydos@mail.ru>
2023-07-14 14:54:49 +0300

    Added Swagger/SpringDoc

--- src/main/java/com/example/todo_list/Controllers/ToDoController.java
+++ src/main/java/com/example/todo_list/Controllers/ToDoController.java
@@ -2,6 +2,9 @@ package com.example.todo_list.Controllers;
 
 import com.example.todo_list.Model.Todo;
 import com.example.todo_list.Services.TodoServices;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,29 +18,37 @@ import java.util.List;
 @Setter
 @RestController
 @RequestMapping(path = "/api/todos", produces = "application/json")
+@Tag(name = "Задачи", description = "Методы для работы с задачами")
 public class ToDoController {
 
     private final TodoServices toDoServices;
 
     @GetMapping
+    @Operation(summary = "Получить все задачи")
     public List<Todo> getAllTodo(){
         return toDoServices.responseAllTodos();
     }
 
     @PostMapping(consumes = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Создать новую задачу")
     public Todo createTodo(@RequestBody Todo todo){
         return toDoServices.createTodo(todo.getName());
     }
 
     @PutMapping(path = "/{id}")
-    public Todo updateTodo(@PathVariable Long id, @RequestBody Todo todo){
+    @Operation(summary = "Обновить существующую задачу по id")
+    public Todo updateTodo(
+            @Parameter(description = "Уникальный идентификатор задачи") @PathVariable Long id,
+            @RequestBody Todo todo){
         return toDoServices.updateTodo(id, todo);
     }
 
     @DeleteMapping(path = "/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public void deleteTodo(@PathVariable Long id){
+    @Operation(summary = "Удалить задачу по id")
+    public void deleteTodo(
+            @Parameter(description = "Уникальный идентификатор задачи") @PathVariable Long id){
         toDoServices.deleteTodo(id);
     }
 }
--- src/main/java/com/example/todo_list/Model/Todo.java
+++ src/main/java/com/example/todo_list/Model/Todo.java
@@ -1,5 +1,6 @@
 package com.example.todo_list.Model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,18 +14,22 @@ import java.util.Date;
 @Data
 @Entity
 @NoArgsConstructor
+@Schema(description = "Информация о задаче")
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Schema(description = "Идентификатор задачи")
     private Long id;
 
     public Todo(@NotNull String name) {
         this.name = name;
     }
 
+    @Schema(description = "Описание задачи")
     @NotNull
     @Size(min = 5)
     private String name;
 
+    @Schema(description = "Дата создания задачи")
     private Date createdAt = new Date();
 }
